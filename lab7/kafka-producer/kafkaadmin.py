b482ef099582b7efc84bb9e9cf3dd5d756035bc6
Tom van Eijk <tomvaneijk8.tve@gmail.com>
2022-02-11 21:52:07 +0100

    Change project name

--- lab7/kafka-producer/kafkaadmin.py
+++ lab7/kafka-producer/kafkaadmin.py
@@ -11,7 +11,7 @@ def create_topics(admin, topic_list):
 
 if __name__ == '__main__':
     admin_client = KafkaAdminClient(bootstrap_servers="34.72.112.43:9092",
-                                    client_id='Lab8')  # use your VM's external IP Here!
+                                    client_id='Lab7')  # use your VM's external IP Here!
     topic_list = [NewTopic(name="word", num_partitions=1, replication_factor=1),
                   NewTopic(name="wordcount", num_partitions=1, replication_factor=1)]
     create_topics(admin_client, topic_list)
